1eebb05c2a6c4d212a11f978cc234ed2db3354db
Adriano Hernandez <adrianoh@mit.edul>
2021-04-19 11:07:12 -0400

    mnist dataset is now functional we also have gitignore of the downloaded files

--- datasets/mnist_dataset.py
+++ datasets/mnist_dataset.py
@@ -1,10 +1,12 @@
 import torch
-from torch.utils.data import Dataloader
+import torchvision
+from torch.utils.data import DataLoader
 from torchvision.transforms import Compose, ToTensor, Normalize
 
 # this will be used to transform the image into
 # something that we can feed into our NN
-image_transform = Compose([ToTensor(), Normalize((0.1307,), (0.3081,))])
+# consider applying: Normalize((0.1307,), (0.3081,)), it was in the code I based this on
+image_transform = Compose([ToTensor()])
 
 # datasets basically are a sort of list datastructure we can query for specific images and labels
 train_dataset = torchvision.datasets.MNIST(
@@ -25,7 +27,45 @@ def get_test_dataloader(batch_size=1024, shuffle=True, **kwargs):
     return DataLoader(test_dataset, batch_size=batch_size, shuffle=shuffle, **kwargs)
 
 
-# TODO for this file
-# 1. add methods to be able to pull out sub-datasets as dictionaries mapping labels
-#     to their corrresponding data (maybe like 20 examples per or 50 examples per)
-# 2. make sure download goes to the right folders
+def get_sub_dataset(num_each=20, train=False):
+    dataset = train_dataset if train else test_dataset
+    sub_dataset = {label: [] for label in range(0, 10)}  # 0 - 9 inclusive
+
+    remaining = set(sub_dataset.keys())
+    for data, label in dataset:
+        sub = sub_dataset[label]
+        if len(sub) >= num_each and label in remaining:
+            remaining.remove(label)
+        elif len(sub) < num_each:
+            sub.append(data)
+
+    # kept as debugging
+    # for k, v in sub_dataset.items():
+    #     print(k, [vv.shape for vv in v])
+
+    return sub_dataset
+
+
+# minimalistic debugging here in main
+if __name__ == "__main__":
+    print("testing get_sub_dataset")
+    get_sub_dataset(num_each=4, train=False)
+    print("ok!\n")
+
+    print("displaying the dataset characteristics")
+    for dataset, name in [(train_dataset, "train"), (test_dataset, "test")]:
+        print("dataset", name)
+        # 1 x 28 x 28
+        # (tensor, label); label is an int
+        print("first elem", dataset[0][0].shape, dataset[0][1])
+
+        # labels are 0 - 9
+        print("min label", min(map(lambda tp: tp[-1], dataset)))
+        print("max label", max(map(lambda tp: tp[-1], dataset)))
+        print("dataset size", len(dataset))
+        print("ok!\n")
+
+    print("testing dataloaders")
+    tdl = get_train_dataloader()
+    ttdl = get_test_dataloader()
+    print("ok!")
