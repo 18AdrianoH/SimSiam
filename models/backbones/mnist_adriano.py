311a132f5c191b26a69c4b5c54a41e1e4aa33e2e
Adriano Hernandez <adrianoh@mit.edul>
2021-04-19 12:21:14 -0400

    ye

--- models/backbones/mnist_adriano.py
+++ models/backbones/mnist_adriano.py
@@ -19,44 +19,54 @@ class SimpleCNN(nn.Module):
     some edits to get it to run nicely for our purposes.
     """
 
-    MNIST_IMG_SIZE = (28, 28)
+    MNIST_IMG_SIZE = (1, 28, 28)
 
     def __init__(self, image_size=None, num_classes=10):
         super(SimpleCNN, self).__init__()
 
         self.image_size = image_size if image_size else SimpleCNN.MNIST_IMG_SIZE
         if len(self.image_size) == 3:
-            self.W, self.H, self.C = self.image_size
+            self.C, self.H, self.W = self.image_size
         elif len(self.image_size) == 2:
-            self.W, self.H = self.image_size
+            self.H, self.W = self.image_size
             self.C = None
 
         self.num_classes = num_classes
+
+        # we'll use stride 1
+        self.conv1_kernel = 5
+        self.conv2_kernel = 5
 
         self.conv1_in_c = self.C if self.C else 1
         self.conv1_out_c = 16
         self.conv2_in_c = self.conv1_out_c
         self.conv2_out_c = 32
 
-        self.fc1_in = self.conv2_out_c * self.H / 4 * self.W / 4  # TODO
+        # there will be 2 max pools so we lose 4x from both height and width
+        # then for the number of edges will be that (the matrix size)
+        # times the number of channels
+        h_c = (self.H - self.conv1_kernel + 1) // 2
+        h_c = (h_c - self.conv2_kernel + 1) // 2
+
+        w_c = (self.W - self.conv1_kernel + 1) // 2
+        w_c = (w_c - self.conv2_kernel + 1) // 2
+
+        self.fc1_in = self.conv2_out_c * h_c * w_c
         self.fc1_out = 64
         self.fc2_in = self.fc1_out
         self.fc2_out = self.num_classes
 
-        # convolution layers process the image initially
-        # they will use zero padding by default to maintain the dimension
         self.conv1 = nn.Conv2d(
-            self.conv1_in_c, self.conv1_out_c, kernel_size=5, stride=1
+            self.conv1_in_c, self.conv1_out_c, kernel_size=self.conv1_kernel, stride=1
         )
+
         self.conv2 = nn.Conv2d(
-            self.conv2_in_c, self.conv2_out_c, kernel_size=5, stride=1
+            self.conv2_in_c, self.conv2_out_c, kernel_size=self.conv2_kernel, stride=1
         )
+
         self.conv2_drop = nn.Dropout2d()
 
-        # second fc creates some sort of representation
         self.fc1 = nn.Linear(self.fc1_in, self.fc1_out)
-
-        # second fully connected layer creates the classes
         self.fc2 = nn.Linear(self.fc2_in, self.fc2_out)
 
     def forward(self, x, reshape=False):
@@ -72,47 +82,33 @@ class SimpleCNN(nn.Module):
 
         # in dimension is (N, conv1_in_c, H, W)
         x = self.conv1(x)
+        # print("After conv 1: {}".format(x.shape))
         x = F.max_pool2d(x, 2)
         x = F.relu(x)
+        # print("After max pool and relu: {}".format(x.shape))
         # out_dimension is now (N, conv1_out_c, H / 2, W / 2)
 
         # in dimension is ibid
         x = self.conv2(x)
+        # print("After conv 2: {}".format(x.shape))
         x = self.conv2_drop(x)
         x = F.max_pool2d(x, 2)
         x = F.relu(x)
+        # print("After conv 2 drop, max pool and relu: {}".format(x.shape))
         # out dimension is (N, conv2_out_c, H / 4 , W / 4)
 
-        # this one will create the "representation"
-        x = x.view((-1, self.fc_in))
+        # shape will now be horizontal
+        # shape (1 x (h / 4 x w / 4 x 32)) = (1 x (h x w x 8))
+        x = x.view((-1, self.fc1_in))
         x = self.fc1(x)
         x = F.relu(x)
         x = F.dropout(x)
 
-        # this last one will create the classes
         x = self.fc2(x)
-
-        return F.log_softmax(x)
+        return x
 
     def fc2_weights(self):
         return self.fc2.detach().cpu().clone()
-
-    def reshape_input(x, batch_size=None, C=None, H=None, W=None):
-        # NOTE: this will break if the dimensions don't match the original expected dims
-        # it is recommended that you pass in C, H, W to confirm
-        for expected, gotten, name in filter(
-            lambda tp: tp[1], [(self.C, C, "C"), (self.H, H, "H"), (self.W, W, "W")]
-        ):
-            if expected != gotten:
-                raise ValueError(
-                    "Got {} for {}, but network is configured for {}".format(
-                        gotten, name, expected
-                    )
-                )
-
-        batch_size = batch_size if batch_size else 1
-
-        return x.view((batch_size, self.C, self.H, self.W))
 
     def train(
         self,
@@ -123,6 +119,7 @@ class SimpleCNN(nn.Module):
         device="cpu",
         num_epochs=64,
         train_name="mnist",
+        save_dir="mnist_testing/",
     ):
         # NOTE: look into pytorch lightning: https://www.pytorchlightning.ai/
         # it will make our lives easier in the future if we continue to implement stuff
@@ -139,30 +136,35 @@ class SimpleCNN(nn.Module):
         train_accs, test_accs = [], []
 
         for epoch in range(1, num_epochs + 1):
+            print("Epoch {}".format(epoch))
+
             train_losses.append(0)
             train_accs.append(0)
             test_losses.append(0)
             test_accs.append(0)
 
             # ugh if only we could have a pointer to an int
-            for dataloader, losses, accs in [
-                (train_dataloader, train_losses, train_accs),
-                (test_dataloader, test_losses, test_accs),
+            for dataloader, losses, accs, dataloader_name in [
+                (train_dataloader, train_losses, train_accs, "train"),
+                (test_dataloader, test_losses, test_accs, "test"),
             ]:
-                for data, target in dataloader:
-                    data, target = data.to(device), target.to(device)
+                for batch, targets in dataloader:
+                    batch, targets = batch.to(device), targets.to(device)
 
                     optimizer.zero_grad()
-                    output = self(data)
-                    pred = torch.argmax(output, 1)
 
-                    loss = F.nll_loss(output, target)
+                    reps = self(batch)
+                    probs = F.log_softmax(reps, dim=1)
+                    preds = torch.argmax(probs, 1)
+
+                    loss = F.nll_loss(probs, targets)
 
                     losses[-1] += loss.item()
-                    accs[-1] += torch.mean((pred == target).type(torch.FloatTensor))
+                    accs[-1] += torch.mean((preds == targets).type(torch.FloatTensor))
 
-                    loss.backward()
-                    optimizer.step()
+                    if dataloader_name == "train":
+                        loss.backward()
+                        optimizer.step()
 
             train_losses[-1] /= len(train_dataloader)
             train_accs[-1] /= len(train_dataloader)
@@ -171,7 +173,7 @@ class SimpleCNN(nn.Module):
             test_accs[-1] /= len(test_dataloader)
 
             test_epoch_loss, train_epoch_loss = test_losses[-1], train_losses[-1]
-            test_epoch_acc, train_epoch_acc = test_accs[-1], train_losses[-1]
+            test_epoch_acc, train_epoch_acc = test_accs[-1], train_accs[-1]
 
             if writer:
                 # the colors might be a tad busted
@@ -184,11 +186,10 @@ class SimpleCNN(nn.Module):
                     {"test": test_epoch_acc, "train": train_epoch_acc},
                 )
 
-            if epoch % 10 == 0:
-                print("Epoch {}".format(epoch))
+            if epoch % 10 == 0 or epoch == 2 or epoch == num_epochs:
                 print(
                     "\tAt {} train loss and {} test loss.".format(
-                        test_epoch_loss, test_epoch_acc
+                        train_epoch_loss, test_epoch_loss
                     )
                 )
                 print(
@@ -198,14 +199,13 @@ class SimpleCNN(nn.Module):
                 )
                 print("")
 
-        # TODO save the parameters or the model somewhere! save the state dict probably
+        torch.save(self, save_dir + "/model.pt")
+        torch.save(self.state_dict(), save_dir + "/state_dict.pt")
+
         return train_losses, test_losses, train_accs, test_accs
 
 
-def test_mnist(self, test_dir=None):
-    # NOTE: it's important to test that this model can achieve good results on the MNIST
-    # dataset before we start using it as an encoder basically
-
+def test_mnist():
     import os
     import shutil
     import sys
@@ -213,28 +213,43 @@ def test_mnist(self, test_dir=None):
     sys.path.append("../../")
 
     from tensorboardX import SummaryWriter
-    from datasets.mnist_dataset import train_dataloader, test_dataloader
+    from datasets.mnist_dataset import get_train_dataloader, get_test_dataloader
 
-    root_dir = "../../outputs"
-    test_dir = "../../outputs/tensorboard"
-    results_dir = "../../outputs/"
+    root_dir = "mnist_testing/"
+    tensorboard_dir = root_dir + "/tensorboard"
+
     if not os.path.isdir(root_dir):
         os.mkdir(root_dir)
-    if os.path.isdir(test_dir):
-        shutil.rmtree(test_dir)
-    os.mkdir(test_dir)
+    if os.path.isdir(tensorboard_dir):
+        shutil.rmtree(tensorboard_dir)
+    os.mkdir(tensorboard_dir)
 
-    writer = SummaryWriter(logdir=test_dir)
+    writer = SummaryWriter(logdir=tensorboard_dir)
 
-    # TODO implement training and logging while you train
-    raise NotImplementedError
+    train_dataloader = get_train_dataloader()
+    test_dataloader = get_test_dataloader()
+    model = SimpleCNN().to("cpu")
+
+    learning_rate = 0.01
+    momentum = 0.5
+    opt = optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
+
+    # it's slow... this is just a test!
+    model.train(
+        train_dataloader,
+        test_dataloader,
+        optimizer=opt,
+        writer=writer,
+        save_dir=root_dir,
+        num_epochs=11,
+    )
+    print("Finished training!")
 
 
 if __name__ == "__main__":
-    test_mnist(self)
+    test_mnist()
     # TODO goals for this file
     # 1. get a test run
     # 2. figure out how to run testing and prediction using main and linear_eval
     # 3. make it possible to extract the features
     # 4. make sure you are using the right folders
-    raise NotImplementedError
